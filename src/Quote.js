10285341d3e19aaf3c10b0b9faa00434290c546b
Rachel Dooley <102691550+Rae1821@users.noreply.github.com>
2023-03-15 09:49:10 -0400

    Fixed heading and landmark issues

--- src/Quote.js
+++ src/Quote.js
@@ -6,17 +6,15 @@ export default function Quote(props) {
     let w = window.innerWidth
 
     return (
-        <div>
-            <div className="quote--container">
-                <h5>Advice #{props.id}</h5>
-                <h2>"{props.advice}"</h2>
-                <img className="divider--mobile"        
-                src={w < 900 ? props.mobileImg : props.desktopImg}
-                alt="pattern divider" 
-                />
-            </div>
-        </div>
 
+        <div className="quote--container">
+            <h5>Advice #{props.id}</h5>
+            <h1>"{props.advice}"</h1>
+            <img className="divider--mobile"        
+            src={w < 900 ? props.mobileImg : props.desktopImg}
+            alt="pattern divider" 
+            />
+        </div>
     )
 
 
